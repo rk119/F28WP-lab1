01e72674739285ab3817d7285857497ae43ee900
Riffat <83776178+rk119@users.noreply.github.com>
2021-09-18 14:38:36 +0400

    Update app.js
    Added a comment about the function

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const num = 100
-let square = (function(a) {
+let square = (function(a) { //this is an anonymous function called immediatly when declared
     return a*a
 })(num)
 console.log(square)
